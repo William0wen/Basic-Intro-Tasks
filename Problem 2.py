c46b3dd22dcb1ba0951ced886de3636d8c6043bb
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-07 10:15:23 +1300

    Update Problem 2.py

--- Problem 2.py
+++ Problem 2.py
@@ -1,11 +1,41 @@
 fish = ["flounder", "sole", "blue cod", "snapper", "terakihi", "john dory", "red cod"]
 
+print("First letter:") #A
+
 for thing in fish:
     print(thing[0])
 
-print("")
+print("\nFirst 3 letters:") #B
 
 for thing in fish:
     print(thing[0:3])
 
-print("")
+print("\nLongest name:") #C
+
+fish_len = sorted(fish, key=len)
+print(fish_len[-1])
+
+print("\nMore than 1 word:") #D
+
+space_names = []
+
+for name in fish:
+    if " " in name:
+        space_names.append(name)
+
+for name in space_names:
+    print(name)
+
+print("\nCod in name:") #E
+
+cod_names = []
+
+for name in fish:
+    if "cod" in name:
+        cod_names.append(name)
+
+for name in cod_names:
+    print(name)
+
+
+
