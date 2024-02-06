a1423df2bf659ec9150db2025ff958c957b6f4ec
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-07 10:22:41 +1300

    Create Problem 3.py

---
+++ Problem 3.py
@@ -0,0 +1,15 @@
+e_counter = 0
+sentence_list = []
+
+
+sentence = input("Type a sentence: ")
+
+sentence_list = sentence.split
+
+print(sentence_list)
+
+for letter in sentence_list:
+    if letter == "e":
+        e_counter += 1
+
+print(e_counter)
