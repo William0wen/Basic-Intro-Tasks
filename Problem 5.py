94c9dd3c523d68c1c7eb96b696d22809c03a9d7d
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-08 13:46:26 +1300

    Create Problem 5.py

---
+++ Problem 5.py
@@ -0,0 +1,8 @@
+numbers = [20, 36, 12, 24, 20, 48, 74, 353, 23, 98]
+
+for number in range(len(numbers)):
+    if numbers[number] == 353:
+        numbers[number] = 35
+
+for number in numbers:
+    print(number)
