0ecfd1405eeeb979e56ff10e7789b91620731b34
William Owen <98859927+William0wen@users.noreply.github.com>
2024-02-08 13:37:41 +1300

    Update Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,15 +1,11 @@
 e_counter = 0
-sentence_list = []
-
 
 sentence = input("Type a sentence: ")
 
-sentence_list = sentence.split
-
-print(sentence_list)
+sentence_list = list(sentence)
 
 for letter in sentence_list:
     if letter == "e":
         e_counter += 1
 
-print(e_counter)
+print(f"The sentence contains 'e' {e_counter} times.")
